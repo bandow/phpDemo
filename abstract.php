71465aba6c9cae48bb01af40e1ee6f1c6296c9b2
goSnowbank <sycbbb@sina.com>
2017-02-12 12:27:37 +0800

    php

---
+++ abstract.php
@@ -0,0 +1,39 @@
+<?php include 'header.php';?>
+
+<?php
+   abstract class AbstractClass{
+		//强制要求子类定义这些方法
+		abstract protected function getValue();
+		abstract protected function prefixValue($prefix);
+    	// 普通方法（非抽象方法）
+		public function printOut(){
+			print $this->getValue() . PHP_EOL;
+		}
+   }
+   class ConcreteClass1 extends AbstractClass{
+   		protected function getValue(){
+   			return "ConcreteClass1";
+   		}
+   		public function prefixValue($prefix){
+   			return "{$prefix}ConcreteClass1";
+   		}
+   } 
+   class ConcreteClass2 extends AbstractClass{
+   		protected function getValue(){
+   			return "ConcreteClass2";
+   		}
+   		public function prefixValue($prefix){
+   			return "{$prefix}ConcreteClass2";
+   		}
+   }
+
+   $class1=new ConcreteClass1;
+   $class1->printOut();
+   echo $class1->prefixValue('AA_') . PHP_EOL;
+
+   $class2=new ConcreteClass2;
+   $class2->printOut();
+   echo $class2->prefixValue('BB_') .PHP_EOL;
+?>
+
+<?php include 'footer.php';?>
---
+++ const.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; ?>
+<!-- 
+常量
+可以把在类中始终保持不变的值定义为常量。在定义和使用常量的时候不需要使用 $ 符号。
+常量的值必须是一个定值，不能是变量，类属性，数学运算的结果或函数调用。
+自 PHP 5.3.0 起，可以用一个变量来动态调用类。但该变量的值不能为关键字（如 self，parent 或 static）。 -->
+
+<?php 
+  class myClass2{
+  	const constant='123456789';
+    function showConstant(){
+    	echo self::constant . PHP_EOL;
+    }
+  }
+  echo myClass2::constant . PHP_EOL;
+
+  $classname2="myClass2";
+  echo $classname2::constant . PHP_EOL;
+
+  $class2=new myClass2;
+  $class2->showConstant();
+  
+  echo $class2::constant .PHP_EOL;
+?>
+
+<?php include 'footer.php'; ?>
